5f1b6f4115f0e7b551a180d4a76b3258517dcf06
Vanja Wallner <Vanja@Vanjas-MacBook-2.local>
2017-02-17 15:53:03 +0100

    new stuff

---
+++ webpage/js/webmenu.js
@@ -0,0 +1,50 @@
+var food = [
+    {
+        "name": "Hamburgare",
+        "price": 67,
+        "kcal": 580,
+        "lactose": true,
+        "gluten": true,
+        "img": "http://www.drodd.com/images15/burger15.png"
+    },
+    {
+        "name": "Ostburgare",
+        "price": 72,
+        "kcal": 580,
+        "lactose": true,
+        "gluten": true,
+        "img": "http://www.cafe.se/wp-content/uploads/2015/02/meat-burger.jpg"
+    },
+    {
+        "name": "Vegoburgare",
+        "price": 60,
+        "kcal": 580,
+        "lactose": false,
+        "gluten": false,
+        "img": "http://cdn1.icakuriren.se/YTo2OntzOjI6ImlkIjtpOjExODkwNTU7czoxOiJ3IjtpOjQ4MDtzOjE6ImgiO2k6MzIwO3M6MToiYyI7aTowO3M6MToicyI7aTowO3M6MToiayI7czo0MDoiMjI5MDVjN2Q3Y2Q3NjliYzU4YmY1YjZkNTkyY2EyM2M1MTZiZWRlOCI7fQ=="
+    },
+    {
+        "name": "Kycklingburgare",
+        "price": 65,
+        "kcal": 580,
+        "lactose": true,
+        "gluten": true,
+        "img": "http://www.ica.se/imagevaultfiles/id_93688/cf_6901/kycklingburgare-med-koriander-och-guacamole-717486.jpg"
+    },
+    {
+        "name": "Nachos",
+        "price": 62,
+        "kcal": 640,
+        "lactose": true,
+        "gluten": false,
+        "img": "http://tacobueno.com/media/1378/muchonacholarge.png?quality=65"
+    },
+    {
+        "name": "Sallad",
+        "price": 59,
+        "kcal": 490,
+        "lactose": false,
+        "gluten": true,
+        "img": "http://ciaoitalia.se/wp-content/uploads/2015/02/Grekisk-sallad.jpg"
+    }
+];
