493f5263f89179bad1b5148d5f6b0c26e27ec891
maxperea <maxpereaduring@gmail.com>
2017-03-04 14:09:01 +0100

    fixat så att arrayen visas nere på sidan

--- nysida/public/js/diner.js
+++ nysida/public/js/diner.js
@@ -9,80 +9,88 @@ function getOrderNumber() {
 }
 
 new Vue({
-el: '.ordering',
-mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
-data: {
-mainDish: [],
-extras: []
-},
-methods: {
-placeOrder: function() {
-// Here two ways of getting selected items are illustrated
-// 1. The Vue way, notice the data model declarations above
-var mainCourse = this.mainDish;
-var extras = this.extras;
-// 2. The old-school way: create an array with values of checked items
-var theRest = [].filter.call(document.getElementsByName('item[]'), function(i) {
-    return i.checked;
-    }).map(function(i) {
-      return i.value;
+  el: '.ordering',
+  mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
+  data: {
+    mainDish: [],
+    extras: []
+  },
+  methods: {
+    placeOrder: function() {
+      // Here two ways of getting selected items are illustrated
+      // 1. The Vue way, notice the data model declarations above
+      var mainCourse = this.mainDish;
+      var extras = this.extras;
+      // 2. The old-school way: create an array with values of checked items
+      var theRest = [].filter.call(document.getElementsByName('item[]'), function(i) {
+        return i.checked;
+      }).map(function(i) {
+        return i.value;
       });
-// OK, it's not really neat to use two different ways of accomplishing the same thing
-// but let's pretend it's for an educational purpose ... here comes another no-no:
-var orderItems = mainCourse.concat(extras).concat(theRest);
-// Finally we make use of socket.io's magic to send the stuff to the kitchen
-socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
-            }
-         }
+      // OK, it's not really neat to use two different ways of accomplishing the same thing
+      // but let's pretend it's for an educational purpose ... here comes another no-no:
+      var orderItems = mainCourse.concat(extras).concat(theRest);
+      // Finally we make use of socket.io's magic to send the stuff to the kitchen
+      socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
+    }
+  }
 });
 
 var summary = [];
 
 function foodSummary(key){
-        summary.push(key);
-        console.log(summary);
-        printSummary();
-        return;
+  summary.push(key);
+  console.log(summary);
+  printSummary();
+  return;
 }
 
 
 function extraSummary(key){
   for(i = 0; i < sideorders.length ; i++){
-     if(sideorders[i].label == key){
-        summary.push(sideorders[i]);
-        printSummary();
-        return;
+    if(sideorders[i].label == key){
+      summary.push(sideorders[i]);
+      printSummary();
+      return;
     }
   }
   console.log('Side order does not exist');
 }
 
 function drinkSummary(key){
-   for(i = 0; i < commondrinks.length ; i++){
-     if(commondrinks[i].label == key){
-        summary.push(commondrinks[i]);
-        printSummary();
-        return;
+  for(i = 0; i < commondrinks.length ; i++){
+    if(commondrinks[i].label == key){
+      summary.push(commondrinks[i]);
+      printSummary();
+      return;
     }  
-  for(i = 0 ; i < drinks.length; i++){
-    if(drinks[i].label == key){
+    for(i = 0 ; i < drinks.length; i++){
+      if(drinks[i].label == key){
         summary.push(drinks[i]);
         printSummary();
         return;
+      }
+
     }
-
+    console.log('Drink does not exist');
   }
-  console.log('Drink does not exist');
-}
 
+}
 
 function printSummary()  {
-var el1 = document.getElementByID(foodlist);
-for(i = 0; i < summary.length; i++){
-var order = document.createElement('li');
-order.setAttribute('value', summary[i]); 
-el1.appendChild(order);
-}
+  //var el1 = document.getElementById(foodlist);
+  document.getElementById("foodlist").innerHTML = ' ';
+  var i;
+  for(i = 0; i < summary.length; i++){
+    //var order = document.createElement('li');
+    //order.setAttribute('value', summary[i]); 
+    //el1.appendChild(order);
+    var node = document.createElement("LI");                 // Create a <li> node
+    node.setAttribute('value', summary[i]); 
+    var textnode = document.createTextNode(summary[i]);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("foodlist").appendChild(node);     // Append <li> to <ul> with id="myList"
+  }
 }
 
 /*
@@ -93,7 +101,6 @@ drink1.setAttribute('value', drinks[i]);
 el2.appendChild(drink1);
 }
 */
-}
 
 
 
